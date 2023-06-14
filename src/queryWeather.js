f55ba8de1ca70b4688dd961f2816d84218013407
Miguel  de la Mata <sebidelamata@gmail.com>
2023-06-14 05:59:20 -0600

    webpack settings

--- src/queryWeather.js
+++ src/queryWeather.js
@@ -1,7 +1,7 @@
 async function queryWeather(_inputString) {
   try {
     let response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=bdea5caba7994efa8da210516230406&q=${_inputString}`,
+      `https://api.weatherapi.com/v1/current.json?key=bdea5caba7994efa8da210516230406&q=${_inputString}`,
       { mode: "cors" }
     );
     let data = await response.json();
